a85990dea6bf587276f37aee76b32887d576f6a7
SerhiiOd <115508914+SerhiiOd@users.noreply.github.com>
2023-06-02 02:03:04 +0300

    ...

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 // import Button from 'components/Button/Button';
 import { Component } from 'react';
 
-export default class App extends Component {
+export class App extends Component {
   render() {
     return <div></div>;
   }
